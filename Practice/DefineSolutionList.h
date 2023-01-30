0929eff2cd9ac486dd65583a11c29e76f99b8c4e
DESKTOP-LJ2P22R\DongHun Yun <yhsk1218@naver.com>
2023-01-31 00:51:21 +0900

    코드 업데이트

---
+++ Practice/DefineSolutionList.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//#define 개인정보수집유효기간
+//#define 크기가작은부분문자열
+//#define 가장가까운같은글자
+//#define 문자열나누기
+//#define 명예의전당1
+//#define 기사단원의무기
+//#define 과일장수
+//#define 무인도여행
---
+++ Practice/Practice.cpp
@@ -0,0 +1,431 @@
+﻿#include "DefineSolutionList.h"
+#include <iostream>
+
+#ifdef 개인정보수집유효기간
+
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
+unordered_map<char, int> mTerms;
+vector<int> priv;
+int year;
+int month;
+int day;
+int total;
+
+vector<int> solution(string today, vector<string> terms, vector<string> privacies) 
+{
+    vector<int> answer;
+
+    // 날짜 계산
+    year = stoi(today.substr(0, 4));
+    month = stoi(today.substr(5, 2));
+    day = stoi(today.substr(8, 2));
+    total = year * 12 * 28 + (month - 1) * 28 + day;
+
+    // 약관 자르기
+    for (int i = 0; i < terms.size(); i++)
+    {
+        char temp; int month;
+        stringstream ss(terms[i]);
+        ss >> temp >> month;
+        mTerms[temp] = month;
+    }
+
+    for (int i = 0; i < privacies.size(); i++)
+    {
+        int y = stoi(privacies[i].substr(0, 4));
+        int m = stoi(privacies[i].substr(5, 2));
+        int d = stoi(privacies[i].substr(8, 2));
+        char a = privacies[i].back();
+
+        int tmp = y * 12 * 28 + (m - 1) * 28 + d + (mTerms[a] * 28 - 1);
+
+        priv.push_back(tmp);
+    }
+
+    for (int i = 0; i < priv.size(); i++)
+    {
+        if (priv[i] < total) {
+            answer.push_back(i + 1);
+        }
+    }
+
+    return answer;
+}
+
+#endif // 개인정보수집유효기간
+
+#ifdef 크기가작은부분문자열
+#include <string>
+#include <vector>
+
+
+int solution(string t, string p)
+{
+    int answer = 0;
+    int tSize = t.size();
+    int pSize = p.size();
+
+    for (int i = 0; i < tSize - pSize + 1; i++)
+    {
+        string s1 = t.substr(i, pSize); // 비교할 부분 string
+        bool same = true;
+
+        for (int j = 0; j < pSize; j++)
+        {
+            if (s1[j] < p[j])
+            {
+                answer++;
+                same = false;
+                break;
+            }
+            else if (s1[j] > p[j])
+            {
+                same = false;
+                break;
+            }
+        }
+
+        if (same)
+            answer++;
+    }
+    
+    return answer;
+}
+#endif // 크기가작은부분문자열
+
+#ifdef 가장가까운같은글자
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+vector<int> solution(string s) 
+{
+    vector<int> answer;
+    unordered_map<char, int> map;   // 인덱스 위치 담아줄 map
+
+    // string 전체 돌아줌
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (map.find(s[i]) != map.end())
+        {
+            answer.push_back(i - map[s[i]]);
+            map[s[i]] = i;
+        }
+        else
+        {
+            answer.push_back(-1);   // -1 담아주고
+            map[s[i]] = i;          // 해당 문자를 키값으로 인덱스 최신화
+        }
+    }
+
+    return answer;
+}
+#endif // 가장가까운같은글자
+
+#ifdef 문자열나누기
+#include <string>
+#include <vector>
+
+int solution(string s) 
+{
+    string s1 = s;
+    int answer = 0;
+    int sCount = 1;
+    int dCount = 0;
+    int idx = 1;
+    char tmp = s[0];
+
+    while (idx <= s1.size())
+    {
+        if (sCount == dCount)
+        {
+            answer++;
+            s1.erase(0, idx);
+            sCount = 1;
+            dCount = 0;
+            idx = 1;
+            tmp = s1[0];
+        }
+        else
+        {
+            if (tmp == s1[idx])
+                sCount++;
+            else
+                dCount++;
+
+            idx++;
+        }
+    }
+
+    if (!s1.empty())
+        answer++;
+
+    return answer;
+}
+#endif // 문자열나누기
+
+#ifdef 명예의전당1
+#include <string>
+#include <vector>
+#include <queue>
+
+vector<int> solution(int k, vector<int> score)
+{
+    vector<int> answer;
+    priority_queue<int, vector<int>, greater<int>> pq; // 오름차순
+
+    
+    for (int i = 0; i < score.size(); i++)
+    {
+        if (pq.size() < k)
+        {
+            pq.push(score[i]);
+        }
+        else
+        {
+            if (pq.top() < score[i])
+            {
+                pq.pop();
+                pq.push(score[i]);
+            }
+        }
+        answer.push_back(pq.top());
+    }
+
+    return answer;
+}
+#endif // 명예의전당1
+
+#ifdef 기사단원의무기
+#include <string>
+#include <vector>
+
+int solution(int number, int limit, int power) 
+{
+    int answer = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        int tmp = 1;
+        for (int j = 1; j <= i / 2; j++)
+        {
+            if (i % j == 0)
+                tmp++;
+
+            if (tmp > limit)
+            {
+                tmp = power;
+                break;
+            }
+        }
+
+        answer += tmp;
+    }
+    return answer;
+}
+#endif // 기사단원의무기
+
+#ifdef 과일장수
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int solution(int k, int m, vector<int> score)
+{
+    int answer = 0;
+    vector<int> v = score;
+    sort(v.begin(), v.end(), greater<int>());   // 내림차순 정렬
+    int idx = 0;
+    int min = v[idx];
+
+    // 벡터 사이즈만큼 반복문 돌기
+    while (m <= v.size())
+    {
+        idx = 0;
+        min = v[idx];                 // 현재 상자에 담을 과일중에서 품질이 가장 낮은것
+        for (int j = 0; j < m; j++)     // m개씩 담아주기
+        {
+            if (min > v[j])             // 담아준 과일의 품질이 가장 낮다면
+                min = v[j];             // 최신화
+        }
+
+        answer += min * m;
+        v.erase(v.begin(), v.begin() + m);
+    }
+    //for (int i = 0; i < v.size();)
+    //{
+    //    if (m > v.size())
+    //        break;
+    //
+    //    int min = v[i];                 // 현재 상자에 담을 과일중에서 품질이 가장 낮은것
+    //    for (int j = 0; j < m; j++)     // m개씩 담아주기
+    //    {
+    //        if (min > v[j])             // 담아준 과일의 품질이 가장 낮다면
+    //            min = v[j];             // 최신화
+    //    }
+    //}
+
+    return answer;
+}
+#endif // 과일장수
+
+#ifdef 무인도여행
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+#define MAX 100
+
+using namespace std;
+
+int dx[4] = { 0, 0, 1, -1 };
+int dy[4] = { 1, -1, 0, 0 };
+int visited[MAX][MAX] = { 0, };
+
+int BFS(int n, int m, vector<string> maps);
+
+vector<int> solution(vector<string> maps) 
+{
+    vector<int> answer;
+
+    int X = maps.size();
+    int Y = maps[0].size();
+
+    for (int i = 0; i < maps.size(); i++)
+    {
+        for (int j = 0; j < maps[i].size(); j++)
+        {
+            if (maps[i][j] == 'X')
+            {
+                visited[i][j] = true;
+            }
+            else 
+            {
+                if (!visited[i][j])
+                {
+                    visited[i][j] = true;
+                    answer.push_back(BFS(i, j, maps));
+                }
+            }
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+
+    if (answer.empty())
+        answer.push_back(-1);
+
+    return answer;
+}
+
+int BFS(int n, int m, vector<string> maps)
+{
+    queue<pair<int, int>> q;
+    q.push({ n, m });
+
+    int add = maps[n][m] - '0';
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+
+            if (0 > nx || nx >= maps.size() ||
+                0 > ny || ny >= maps[0].size())
+                continue;
+
+            if (maps[nx][ny] != 'X' && !visited[nx][ny])
+            {
+                visited[nx][ny] = true;
+                add += maps[nx][ny] - '0';
+                q.push({ nx, ny });
+            }
+        }
+    }
+
+    return add;
+}
+#endif // 무인도여행
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+#ifdef 개인정보수집유효기간
+    string today = "2020.01.01";
+    vector<string> terms = 
+    { "Z 3", 
+      "D 5" };
+    vector<string> privacies = 
+    {   "2019.01.01 D",
+        "2019.11.15 Z",
+        "2019.08.02 D",
+        "2019.07.01 D",
+        "2018.12.28 Z" };
+
+    vector<int> answer = solution(today, terms, privacies);
+
+    for (int i = 0; i < answer.size(); i++)
+        cout << answer[i];
+
+    cout << endl;
+
+
+#endif // 개인정보수집유효기간
+#ifdef 크기가작은부분문자열
+    string t = "500220839878";
+    string p = "7";
+
+#endif // 크기가작은부분문자열
+#ifdef 가장가까운같은글자
+    vector<int> answer = solution("baaab");
+
+    for (int i = 0; i < answer.size(); i++)
+        cout << answer[i] << endl;
+
+#endif // 가장가까운같은글자
+#ifdef 문자열나누기
+    cout << solution("abracadabra") << endl;
+    //cout << solution("aaabbaccccabba") << endl;
+#endif // 문자열나누기
+#ifdef 명예의전당1
+
+    vector<int> tmp = { 10, 100, 20, 150, 1, 100, 200 };
+    vector<int> ans = solution(3, tmp);
+    
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << endl;
+
+
+#endif // 명예의전당1
+#ifdef 기사단원의무기
+    cout << solution(10, 3, 2);
+#endif // 기사단원의무기
+#ifdef 과일장수
+    vector<int> score = { 4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2 };
+    cout << solution(4, 3, score);
+
+#endif // 과일장수
+#ifdef 무인도여행
+
+    vector<string> maps = { "X591X","X1X5X","X231X", "1XXX1" };
+    vector<int> ans = solution(maps);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i];
+    }
+
+#endif // 무인도여행
+
+}
